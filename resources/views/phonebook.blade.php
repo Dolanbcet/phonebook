4f4471e54ba080bcc88668c77d4f139d11b11e42
Dolan Pc <dolanbcet@gmail.com>
2018-06-10 00:55:10 +0530

    modal added

---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,21 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Laravel Vue
+        </title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        <div id="app">
+            <Myheader></Myheader>
+            <div class="container">
+            <router-view></router-view>
+            
+            </div>
+            <Myfooter></Myfooter>
+        </div>
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
